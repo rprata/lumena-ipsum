fe2d4b4fe939cb9b333d18066d94be8bf67e9c53
rprata <renan.prata@pagar.me>
2021-03-02 23:20:40 -0300

    adsense: include adsense

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script data-ad-client="ca-pub-3281929352714416" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />         
         </Head>
